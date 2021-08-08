c645bfdc81aa29aba0216337c8e7acd658138f4e
BottlesMan <man2545_@hotmail.com>
2021-08-08 15:49:47 +0700

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { useState } from 'react'
+import Footer from './Footer';
+import Header from './Header';
+import Todo from './Todo';
 
 function App() {
 
@@ -11,26 +12,28 @@ function App() {
     <div>
 
       {/* header section */}
-      <div className='flex justify-center items-end space-x-2'>
+      {/* <div className='flex justify-center items-end space-x-2'>
         <span className='text-center italic my-2 text-2xl'>Minimal Todo List </span>
         <span className='text-gray-400 italic my-2 text-xl'>by ...</span>
-      </div>
+      </div> */}
+      <Header></Header>
 
       {/* todo section */}
-      <div className='mx-auto max-w-4xl'>
+      {/* <div className='mx-auto max-w-4xl'> */}
+      <Todo></Todo>
 
         {/* task input and add button */}
-        <div className='flex space-x-1'>
+        {/* <div className='flex space-x-1'>
           <input className='border border-gray-400 w-full text-2xl'
             onKeyDown={onKeyDownCallback} ></input>
           <button className='border border-gray-400 w-8 font-bold'>+</button>
-        </div>
+        </div> */}
 
         {/* tasks section */}
-        <div>
+        {/* <div> */}
           {/* task example */}
           {/* Please convert this into a task component */}
-          <div
+          {/* <div
             className="flex justify-between h-8 items-center py-6 border-b"
           >
             <span className="text-2xl"> I am a task </span>
@@ -38,10 +41,10 @@ function App() {
               <button className="bg-green-400 w-24 text-2xl" >Done</button>
               <button className="bg-red-400 w-24 text-2xl" >Delete</button>
             </div>
-          </div>
+          </div> */}
 
           {/* another task example */}
-          <div
+          {/* <div
             className="flex justify-between h-8 items-center py-6 border-b"
           >
             <span className="text-2xl"> I am another task </span>
@@ -52,10 +55,11 @@ function App() {
           </div>
 
         </div>
-      </div>
+      </div> */}
 
       {/* footer section */}
-      <p className='text-center text-gray-400'> ... </p>
+      {/* <p className='text-center text-gray-400'> ... </p> */}
+      <Footer></Footer>
     </div>
   );
 }
